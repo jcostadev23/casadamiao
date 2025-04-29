40c717fd00bc49f71ffb40213c39d2641c76c2bc
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-04-29 22:45:03 +0100

    header

---
+++ src/app/components/Main/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <a
+      className="flex items-center justify-center"
+      href="https://casadamiao.pt/2020/04/04/bem-vindo/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <h1 className="text-5xl font-semibold ">Bem-vindo</h1>
+    </a>
+  );
+};
+export default Header;
