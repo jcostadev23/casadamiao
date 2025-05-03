a8171d124ca1c8b8a98bdda4ca5db5e2033da473
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-05-03 13:19:22 +0100

    Adding the main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
+import SideMenu from "./SideMenu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div>
+          <Header />
+          <div className="flex flex-col min-h-screen p-8 pb-20 gap-16 sm:p-20 sm:flex-row font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col flex-1 gap-[32px] row-start-2 w-full sm:w-3/4 sm:items-start">
+              {children}
+            </main>
+            <SideMenu />
+          </div>
+        </div>
       </body>
     </html>
   );
