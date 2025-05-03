c2a2a184fa11929925ca6d4556134def061a24b6
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-05-03 13:20:35 +0100

    Cleaning the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import MarkDown from "./markdown/page.mdx";
+import MainInfo from "./main/page";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[10px_2fr_10px]  min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2  sm:items-start">
-        <MarkDown />
-      </main>
-    </div>
-  );
+  return <MainInfo />;
 }
