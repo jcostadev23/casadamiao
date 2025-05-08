33ef0357129696f1ec9f9e93c86aff68388151df
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-05-08 19:44:19 +0100

    Adding map location component

---
+++ src/app/contatos/Map.tsx
@@ -0,0 +1,15 @@
+const Map = () => {
+  return (
+    <div style={{ width: "100%", height: "500px" }}>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1066.7003675135807!2d-9.124923085198203!3d38.819688161024324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd192d01b31a264f%3A0x215539476984f585!2sCasa%20Dami%C3%A3o!5e0!3m2!1sen!2spt!4v1746729685250!5m2!1sen!2spt"
+        width="100%"
+        height="100%"
+        style={{ border: 0 }}
+        loading="lazy"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Map;
