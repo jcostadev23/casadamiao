6908aeee28c0e0aff1a42b46b6b18489152f8597
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-05-23 11:42:45 +0100

    Correction-on-volutarios-link

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -2,16 +2,6 @@ const Footer = () => {
   return (
     <footer className="flex flex-col gap-[10px] mb-[30px] mt-[40px] items-center justify-center">
       <p className="flex items-center gap-2 ">Casa Damião © 2025</p>
-      <p className="flex items-center gap-2">
-        Alojado por
-        <a
-          className="hover:underline hover:underline-offset-4"
-          href="https://www.amen.pt/"
-          rel="noopener noreferrer"
-        >
-          <span>AMEN.PT</span>
-        </a>
-      </p>
     </footer>
   );
 };
