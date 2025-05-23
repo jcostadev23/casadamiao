14912bb02e6d11ca8c32b5cc0ee96317bec0b37b
Jose Costa <125750186+jcostadev23@users.noreply.github.com>
2025-05-23 12:25:07 +0100

    Update index.tsx

--- src/app/components/NavBar/index.tsx
+++ src/app/components/NavBar/index.tsx
@@ -8,35 +8,13 @@ import Nav from "./Nav";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    let timeoutId: NodeJS.Timeout;
-
-    const handleScroll = () => {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => {
-        setScrolled(window.scrollY > 50);
-      }, 20);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      clearTimeout(timeoutId);
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   return (
     <header
-      className={`mb-6 text-sm bg-menu-background text-foreground ${
-        scrolled
-          ? "text-xs"
-          : "text-sm"
-      }`}
+      className="mb-6 text-sm bg-menu-background text-foreground"
     >
       <div className="flex flex-col justify-between px-4 items-center gap-2 md:flex-row sm:flex-col">
-        <CasaDamiaoImage scrolled={scrolled} />
+        <CasaDamiaoImage />
         <div className="flex-1"></div>
         <Nav />
         <button
