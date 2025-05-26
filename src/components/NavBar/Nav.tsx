d7442307313e2c5813fd1d0dcd29e3c5c5c5b2ab
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-05-26 16:10:19 +0100

    Indentifying the page you are in

--- src/components/NavBar/Nav.tsx
+++ src/components/NavBar/Nav.tsx
@@ -1,19 +1,28 @@
+"use client";
+import { usePathname } from "next/navigation";
 import { DOAR_URL } from "../../utils";
 
 const Nav = () => {
+  const pathName = usePathname();
   return (
     <nav className="hidden gap-6 mr-6 md:gap-4 md:flex lg:flex ">
       <a className="flex" href={DOAR_URL} target="_blank">
         <span className="animate-heartbeat">{"\u2764\uFE0F"} DOAR</span>
       </a>
       <a href={"/a-casa-damiao/"} rel="noopener noreferrer">
-        <span>BEM-VINDO</span>
+        <span className={`${pathName === "/a-casa-damiao" && "opacity-50"}`}>
+          BEM-VINDO
+        </span>
       </a>
       <a href={"/casadamiao/"} rel="noopener noreferrer">
-        <span>SOBRE NÓS</span>
+        <span className={`${pathName === "/casadamiao" && "opacity-50"}`}>
+          SOBRE NÓS
+        </span>
       </a>
       <a href={"/contatos/"} rel="noopener noreferrer">
-        <span>CONTATOS</span>
+        <span className={`${pathName === "/contatos" && "opacity-50"}`}>
+          CONTATOS
+        </span>
       </a>
     </nav>
   );
